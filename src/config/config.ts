3ea87d2d55885d9aba041fa09749fb192980661a
Arabhub-glitch <nullbrodidyouknow@gmail.com>
2026-03-30 11:44:49 +0200

    Update developer IDs and allowed server IDs

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,9 +9,9 @@ export default {
     embedColor: "#06c2fb",
     defaultLanguage: "en",
     debugMode: true,
-  //  allowedServers: ["1158846168957210635", "1399471603003428966"], // Example server IDs
+  //  allowedServers: ["1482872506338644113", "1399471603003428966"], // Example server IDs
     prefix: "!",
-    developers: ["527826654660132890"],
+    developers: ["1408893736192180268"],
     database: {
         type: SupportedDatabaseTypes.MongoDB,
         url: "mongodb://localhost:27017/badge2",
